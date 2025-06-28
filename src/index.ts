a5b506b418233cc1d24d54a5e86ab31a53ee6689
kumarutkarshuk <kumarutkarshuk18@gmail.com>
2025-06-28 20:52:07 +0530

    feat: integrate weather api, implement legacy sse

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
-import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+// import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import z from "zod";
+import { SSEServerTransport } from "@modelcontextprotocol/sdk/server/sse.js";
+import "dotenv/config";
+const express = require("express");
 
 if (!process.env.WEATHER_API_KEY) {
   throw new Error("Weather API key not found");
@@ -33,10 +36,17 @@ const getWeatherByCityName = async (city: string): Promise<WeatherData> => {
   }
 };
 
-const server = new McpServer({
-  name: "mcp-server-weather",
-  version: "1.0.0",
-});
+const app = express();
+
+const server = new McpServer(
+  {
+    name: "mcp-server-weather",
+    version: "1.0.0",
+  },
+  {
+    capabilities: {},
+  }
+);
 
 server.registerTool(
   "getWeatherByCityName",
@@ -52,9 +62,30 @@ server.registerTool(
   })
 );
 
-async function init() {
-  const transport = new StdioServerTransport();
-  await server.connect(transport);
-}
+// async function init() {
+//   const transport = new StdioServerTransport();
+//   await server.connect(transport);
+// }
 
-init();
+// init();
+
+// not working
+// app.post("/mcp", async (req, res) => {
+//   const response = await server.handleRequest(req.body);
+//   res.json(response)
+// });
+
+let transport: SSEServerTransport | null = null;
+
+app.get("/sse", (req, res) => {
+  transport = new SSEServerTransport("/messages", res);
+  server.connect(transport);
+});
+
+app.post("/messages", (req, res) => {
+  if (transport) {
+    transport.handlePostMessage(req, res);
+  }
+});
+
+app.listen(3000);
